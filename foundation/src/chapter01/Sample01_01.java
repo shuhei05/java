86a89219764bc19387d299499ad9a7ecce6d5ded
shuhei05 <qoo0228y@gmail.com>
2021-07-28 22:29:38 +0900

    update:file

--- foundation/src/chapter01/Sample01_01.java
+++ foundation/src/chapter01/Sample01_01.java
@@ -4,7 +4,23 @@ public class Sample01_01 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		
+		int[] numbers ={1, 4, 6, 9, 13, 16}; 
+	    
+	    int oddSum = 0;
+	    int evenSum = 0;
+	    
+	    // for文を用いて、配列numbersの偶数の和と奇数の和を求めてください
+	      for (int number : numbers) {
+	      if (number % 2 == 0) {
+	        evenSum += number;
+	      } else {
+	        oddSum += number;
+	      }
+	    }
 
+	    System.out.println("奇数の和は" + oddSum + "です");
+	    System.out.println("偶数の和は" + evenSum + "です");
 	}
 
 }
